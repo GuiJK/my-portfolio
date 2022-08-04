43f6ade27276d6a2f5288a33adf94f300ffee3ca
GuiJK <junquekarabe18@gmail.com>
2022-08-04 18:37:38 -0300

    meu portfólio

---
+++ src/components/About/Techstack.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  DiJavascript1,
+  DiReact,
+  DiNodejs,
+  DiGit,
+  DiLinux,
+} from "react-icons/di";
+import {
+} from "react-icons/si";
+
+function Techstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiNodejs />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiReact />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiLinux />
+      </Col>
+    </Row>
+  );
+}
+
+export default Techstack;
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+import junqueflix from "../../Assets/Projects/junqueflix.png";
+import pokedex from "../../Assets/Projects/pokedex.png";
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={junqueflix}
+              isBlog={false}
+              title="Junqueflix"
+              description="In this project I created the Netflix home that plays all trailers."
+              ghLink="https://github.com/GuiJK/junqueflix.git"
+              demoLink="https://junqueflix.netlify.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={pokedex}
+              isBlog={false}
+              title="Pokedex"
+              description="A pokedex of the Pokemon cartoon that I made using CSS and HTML."
+              ghLink="https://github.com/GuiJK/css.git"
+            />
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
